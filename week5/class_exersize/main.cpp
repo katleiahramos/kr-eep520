5a2633e10523d1df39d4ff7c95f5deb9e2852744
Katleiah <katleiah@code.org>
2023-02-06 15:49:10 -0800

    Add week 5 class exercises

---
+++ week5/class_exersize/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <set>
+
+
+int main() {
+  // Creating a vector of integers
+  std::vector<int> numbers = {1, 2, 3, 4, 5};
+
+  // Using iterators to traverse a vector
+  std::cout << "Traversing a vectoring using iterators:/n";
+
+  // Declaring an iterator of type std::vector<int>::iterator
+  //iterator
+
+  std::vector<int>::iterator it;
+
+  //pointer
+
+  // Starting a for loop to traverse the vector
+  //iterator
+
+  for( it = numbers.begin(); it != numbers.end(); ++it) {
+    std::cout << *it << " ";
+  }
+
+  return 0;
+  //pointer
+
+  // Printing the value at the current position pointed by the iterator
+  //iterator
+
+  //pointer
+
+  // std::cout << "\n\n";
+
+  // Creating a map of string keys and integer values
+  // std::map<std::string, int> m = {{"apple", 1}, {"banana", 2}, {"cherry", 3}};
+
+  // Using iterators to traverse a map
+
+
+  // Declaring an iterator of type std::map<std::string, int>::iterator
+  //iterator
+
+  //pointer
+
+  // Starting a for loop to traverse the map
+  //iterator
+
+  //pointer
+
+  // Printing the key-value pair at the current position pointed by the iterator
+  //iterator
+
+  //pointer
+
+  }
+  // std::cout << "\n";
+
+  // // Creating a set of integers
+  // std::set<int> s = {1, 2, 3, 4, 5};
+
+  // Using iterators to traverse a set
+
+
+  // Declaring an iterator of type std::set<int>::iterator
+  //iterator
+
+  //pointer
+
+  // Starting a for loop to traverse the set
+  //iterator
+
+  //pointer
+
+  // Printing the value at the current position pointed by the iterator
+  //iterator
+
+  //pointer
+
+//   }
+//   std::cout << "\n";
+
+//   return 0;
+// }
